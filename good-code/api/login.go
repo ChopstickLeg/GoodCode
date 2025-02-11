7dbc72143b4879de2e080b6d472cd0e4a9d372df
Hayden Carpenter <hayden.carpenter@amwins.com>
2025-02-11 16:17:19 -0500

    Attempting to implement Turso db

--- good-code/api/login.go
+++ good-code/api/login.go
@@ -1,26 +1,33 @@
 package handler
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
+	"os"
 
-	"github.com/gorilla/mux"
+	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
 
+var dbUrl = os.Getenv("TURSO_DB_URL")
+var dbAuth = os.Getenv("TURSO_AUTH_TOKEN")
+
 func Handler(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
+
+	conn, err := libsql.Open(dbUrl, libsql.WithAuthToken(dbAuth))
+	if err != nil {
+		http.Error(w, "DB connection failed", http.StatusInternalServerError)
+	}
+	defer conn.Close()
+
 	dataJson := map[string]interface{}{
 		"data": map[string]interface{}{
 			"message": "Hello World!",
 		},
 	}
-	err := json.NewEncoder(w).Encode(dataJson)
+	err = json.NewEncoder(w).Encode(dataJson)
 	if err != nil {
 		http.Error(w, "Failed to encode JSON", http.StatusInternalServerError)
 	}
-}
-
-func Serve() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", Handler).Methods("GET")
-	http.ListenAndServe(":8080", r)
 }
--- good-code/go.mod
+++ good-code/go.mod
@@ -2,4 +2,13 @@ module github.com/chopstickleg/good-code
 
 go 1.23.4
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require github.com/gorilla/mux v1.8.1
+
+require github.com/tursodatabase/libsql-client-go v0.0.0-20240902231107-85af5b9d094d
+
+require (
+	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
+	github.com/libsql/sqlite-antlr4-parser v0.0.0-20240327125255-dbf53b6cbf06 // indirect
+	github.com/tursodatabase/go-libsql v0.0.0-20241221181756-6121e81fbf92 // indirect
+	golang.org/x/exp v0.0.0-20240325151524-a685a6edb6d8 // indirect
+)
