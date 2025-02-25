8ccc669193ee9dd335ffe8d5c851de3657f4daaa
Hayden Carpenter <hayden.carpenter@amwins.com>
2025-02-25 08:38:16 -0500

    Adding Logging

--- good-code/db/db.go
+++ good-code/db/db.go
@@ -5,9 +5,11 @@ import (
 	"log"
 	"os"
 	"sync"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var (
@@ -17,13 +19,27 @@ var (
 
 func GetDB() (*gorm.DB, error) {
 	var err error
+
+	logger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold:             time.Second,
+			LogLevel:                  logger.Info,
+			IgnoreRecordNotFoundError: false,
+			ParameterizedQueries:      false,
+			Colorful:                  true,
+		},
+	)
+
 	dbOnce.Do(func() {
 		dbURL := os.Getenv("DATABASE_DATABASE_URL")
 		if dbURL == "" {
 			err = errors.New("DATABASE_URL environment variable not set")
 		}
 
-		conn, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
+		conn, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{
+			Logger: logger,
+		})
 		if err != nil {
 			log.Fatal("Failed to connect to Neon Postgres:", err)
 		}
