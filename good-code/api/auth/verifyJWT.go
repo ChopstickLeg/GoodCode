8d3fc491978e5ac0c6f44ae987dcc050971cfc61
Hayden Carpenter <hayden.carpenter@amwins.com>
2025-02-26 12:45:58 -0500

    typo

--- good-code/api/auth/verifyJWT.go
+++ good-code/api/auth/verifyJWT.go
@@ -36,7 +36,7 @@ func VerifyJWTHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Content-Type", "application/json")
 	response := json.NewEncoder(w)
-	err = response.Encode(map[string]bool{"loggedIn": false})
+	err = response.Encode(map[string]bool{"loggedIn": true})
 	if err != nil {
 		http.Error(w, "Failed to send response: "+err.Error(), http.StatusInternalServerError)
 		return
