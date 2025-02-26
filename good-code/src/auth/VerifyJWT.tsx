e13fe74047e1c38981026fa0cb3de1dcbebf0d12
Hayden Carpenter <hayden.carpenter@amwins.com>
2025-02-26 13:04:47 -0500

    Adding logging

--- good-code/src/auth/VerifyJWT.tsx
+++ good-code/src/auth/VerifyJWT.tsx
@@ -9,12 +9,14 @@ export const PrivateRoute = ({ children }: { children: JSX.Element }) => {
 
   useEffect(() => {
     if (isError || (data && !data.loggedIn)) {
+      console.log("Login failed, returning to login screen");
       navigate("/login", { state: { from: location }, replace: true });
     }
   }, [data, isError]);
 
-  if (isLoading) return (<div>Loading...</div>); // Show loading state while checking auth
+  if (isLoading) return <div>Loading...</div>;
 
+  console.log("Login successful");
   return children;
 };
 
