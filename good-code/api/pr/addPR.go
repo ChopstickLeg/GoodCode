92f4df4f6383c8a1958edbb53c30ea1c32393c7c
Hayden Carpenter <carpenterhayden1010@gmail.com>
2025-05-20 16:09:19 -0400

    Testing out google AI Package

--- good-code/api/pr/addPR.go
+++ good-code/api/pr/addPR.go
@@ -1,15 +1,15 @@
 package handler
 
 import (
-	"encoding/json"
+	"context"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
-	"strings"
 
-	db "github.com/chopstickleg/good-code/api/_db"
 	middleware "github.com/chopstickleg/good-code/api/_middleware"
+
+	"google.golang.org/genai"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
@@ -38,7 +38,7 @@ func AddPRHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "unable to parse form", http.StatusBadRequest)
 		return
 	}
-	
+
 	file, _, err := r.FormFile("diff")
 	if err != nil {
 		http.Error(w, "Unable to retrieve file", http.StatusBadRequest)
@@ -52,61 +52,75 @@ func AddPRHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-
 	fmt.Println("File content:", string(fileBytes))
 
-	reqBody := db.AI_request {
-		Prompt: string(fileBytes),
-		Model: "gemma3:12b",
-		System: "You are a code review assistant. You will be given a diff of a pull request. Your task is to review the code and provide feedback. You should be sarcastic and condescending, but still helpful and provide useful feedback that is factually accurate to the best of your knowledge",
-		Stream: false,
-	}
-	
-	body, err := json.Marshal(reqBody)
-	if err != nil {
-		http.Error(w, "Unable to marshal request", http.StatusInternalServerError)
-		return
-	}
+	ctx := context.Background()
+	client, err := genai.NewClient(ctx, &genai.ClientConfig{
+		APIKey:  token,
+		Backend: genai.BackendGeminiAPI,
+	})
 
-	req, err := http.NewRequest("POST", url, strings.NewReader(string(body)))
-	if err != nil {
-		http.Error(w, "Unable to send request to AI API", http.StatusInternalServerError)
-		return
-	}
-	req.Header.Set("Authorization", "Bearer "+token)
-	req.Header.Set("Content-Type", "application/json")
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		http.Error(w, "Unable to send request to AI API", http.StatusInternalServerError)
-		return
-	}
-	
-	defer resp.Body.Close()
-	
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "text/plain")
-	aiResponse, err := io.ReadAll(resp.Body)
-	if err != nil {
-		http.Error(w, "Unable to read response from AI API", http.StatusInternalServerError)
-		return
-	}
-	w.Write(aiResponse)
+	result, _ := client.Models.GenerateContent(
+		ctx,
+		"gemini-2.5-flash-preview-05-20",
+		genai.Text(string(fileBytes)),
+		nil,
+	)
 
-	gormdb, err := db.GetDB()
-	if err != nil {
-		fmt.Println("Failed to connect to database:", err)
-		return
-	}
-	pr := db.Pull_request{
-		ID:            0,
-		Author_id:     1,
-		Author_name:   "temp_author",
-		Source_branch: "temp_source",
-		Target_branch: "temp_target",
-		Has_comments:  true,
-		AIComments:    string(aiResponse),
-	}
-	gormdb.AutoMigrate(&db.Pull_request{})
-	gormdb.Create(&pr)
+	fmt.Print(result.Text())
+
+	// reqBody := db.AI_request{
+	// 	Prompt: string(fileBytes),
+	// 	Model:  "gemma3:12b",
+	// 	System: "You are a code review assistant. You will be given a diff of a pull request. Your task is to review the code and provide feedback. You should be sarcastic and condescending, but still helpful and provide useful feedback that is factually accurate to the best of your knowledge",
+	// 	Stream: false,
+	// }
+
+	// body, err := json.Marshal(reqBody)
+	// if err != nil {
+	// 	http.Error(w, "Unable to marshal request", http.StatusInternalServerError)
+	// 	return
+	// }
+
+	// req, err := http.NewRequest("POST", url, strings.NewReader(string(body)))
+	// if err != nil {
+	// 	http.Error(w, "Unable to send request to AI API", http.StatusInternalServerError)
+	// 	return
+	// }
+	// req.Header.Set("Authorization", "Bearer "+token)
+	// req.Header.Set("Content-Type", "application/json")
+	// client := &http.Client{}
+	// resp, err := client.Do(req)
+	// if err != nil {
+	// 	http.Error(w, "Unable to send request to AI API", http.StatusInternalServerError)
+	// 	return
+	// }
+
+	// defer resp.Body.Close()
+
+	// w.WriteHeader(http.StatusOK)
+	// w.Header().Set("Content-Type", "text/plain")
+	// aiResponse, err := io.ReadAll(resp.Body)
+	// if err != nil {
+	// 	http.Error(w, "Unable to read response from AI API", http.StatusInternalServerError)
+	// 	return
+	// }
+	// w.Write(aiResponse)
+
+	// gormdb, err := db.GetDB()
+	// if err != nil {
+	// 	fmt.Println("Failed to connect to database:", err)
+	// 	return
+	// }
+	// pr := db.Pull_request{
+	// 	ID:            0,
+	// 	Author_id:     1,
+	// 	Author_name:   "temp_author",
+	// 	Source_branch: "temp_source",
+	// 	Target_branch: "temp_target",
+	// 	Has_comments:  true,
+	// 	AIComments:    result.Text(),
+	// }
+	// gormdb.AutoMigrate(&db.Pull_request{})
+	// gormdb.Create(&pr)
 }
